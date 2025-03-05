b541298db4cbbf977a77043a2ffab05804fedf54
Alyssa De Los Santos <adelossantos6@gmail.com>
2025-03-05 17:58:36 -0600

    reformat html and css

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("JavaScript is connected!");
 })
+
+function toggleMenu() {
+    document.querySelector(".right").classList.toggle("active");
+}
