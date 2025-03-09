a03699bf2b8d5b2ae198a7e235e247ff3cda2769
Alyssa De Los Santos <adelossantos6@gmail.com>
2025-03-09 15:33:22 -0500

    update js file for ham menu

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("JavaScript is connected!");
-})
+const hamBtn = document.querySelector(".ham-btn");
+const navMenu = document.querySelector(".ham-menu-links");
 
-function toggleMenu() {
-    document.querySelector(".right").classList.toggle("active");
-}
+hamBtn.addEventListener('click', () => {
+    const isOpened = hamBtn.getAttribute('aria-expanded') === 'true';
+
+    hamBtn.setAttribute('aria-expanded', isOpened ? 'false' : 'true');
+    navMenu.classList.toggle('active');
+
+    if (!hamBtn.hasAttribute('aria-expanded')) {
+        hamBtn.setAttribute('aria-expanded', 'false');
+    }
+
+})
